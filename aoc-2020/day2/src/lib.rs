649e518692d35b6fc4131fac838ce5d1bc852158
Victor Johnsson <victor.johnsson@gmail.com>
2021-01-04 05:53:47 +0100

    solved day 2 part 1

--- aoc-2020/day2/src/lib.rs
+++ aoc-2020/day2/src/lib.rs
@@ -1,32 +1,42 @@
 pub fn solve_part_1(input: &String) -> String {
-    let passwords: Vec<Password> = input.trim().split('\n').map(parse_password).collect();
-    return passwords.iter().filter(|p| p.is_valid()).count().to_string();
+    input
+        .trim()
+        .split('\n')
+        .map(parse_password)
+        .filter(Password::is_valid)
+        .count()
+        .to_string()
 }
 
 fn parse_password(text: &str) -> Password {
+    let text: Vec<&str> = text.split(": ").collect();
+    let policy_text: Vec<&str> = text[0].split(|c| c == '-' || c == ' ').collect();
     Password {
         policy: Policy {
-            letter: 'a',
-            min: 1,
-            max: 9,
+            letter: policy_text[2].chars().next().unwrap(),
+            min: policy_text[0].parse::<usize>().unwrap(),
+            max: policy_text[1].parse::<usize>().unwrap(),
         },
-        password: String::from("awd"),
+        password: text[1],
     }
 }
 
-struct Password {
+struct Password<'a> {
     policy: Policy,
-    password: String,
+    password: &'a str,
 }
 
 struct Policy {
     letter: char,
-    min: i32,
-    max: i32,
+    min: usize,
+    max: usize,
 }
 
-impl Password {
-    fn is_valid(&self) -> bool {return true}
+impl Password<'_> {
+    fn is_valid(&self) -> bool {
+        let occurences = self.password.chars().filter(|c| *c == self.policy.letter).count();
+        occurences >= self.policy.min && occurences <= self.policy.max
+    }
 }
 
 pub fn solve_part_2(input: &String) -> String {
