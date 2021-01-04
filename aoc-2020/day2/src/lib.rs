56cde68588ed2afd6e06c50a2afa65f457a75a5a
Victor Johnsson <victor.johnsson@gmail.com>
2021-01-04 05:05:34 +0100

    added skeleton

--- aoc-2020/day2/src/lib.rs
+++ aoc-2020/day2/src/lib.rs
@@ -1,7 +1,34 @@
 pub fn solve_part_1(input: &String) -> String {
-    panic!("no solution found");
+    let passwords: Vec<Password> = input.trim().split('\n').map(parse_password).collect();
+    return passwords.iter().filter(|p| p.is_valid()).count().to_string();
+}
+
+fn parse_password(text: &str) -> Password {
+    Password {
+        policy: Policy {
+            letter: 'a',
+            min: 1,
+            max: 9,
+        },
+        password: String::from("awd"),
+    }
+}
+
+struct Password {
+    policy: Policy,
+    password: String,
+}
+
+struct Policy {
+    letter: char,
+    min: i32,
+    max: i32,
+}
+
+impl Password {
+    fn is_valid(&self) -> bool {return true}
 }
 
 pub fn solve_part_2(input: &String) -> String {
-    panic!("no solution found");
+    String::from("not yet implemented")
 }
